61ccb4b74db8be9a2cfe371a7620f44a7380e414
Chucho <iquarus2@gmail.com>
2026-09-22 21:37:04 -0600

    Toy aburrido

---
+++ ASCII/gato/gato.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int main(){
+
+    SetConsoleOutputCP(CP_UTF8); //Para que soporte porque no me gusta con el circulo normal
+
+ cout<<"___________\n";
+ cout<<"|         |\n";
+ cout<<"|  Putas  |\n";
+ cout<<"|  Aqui   |\n";
+ cout<<"|         |\n";
+ cout<<"|_________|\n";
+ cout<<"(\\__/)||\n";
+ cout<<"(•ㅅ•) ||\n";
+ cout<<"/     づ";
+}
